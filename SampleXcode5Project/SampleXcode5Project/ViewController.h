cb08e31590a9361cba01126898d76ad119efd62c
Shohei SUNAGAWA <kanpe777@gmail.com>
2013-11-13 23:30:54 +0900

    New XcodeProject

---
+++ SampleXcode5Project/SampleXcode5Project/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleXcode5Project
+//
+//  Created by git on 2013/11/13.
+//  Copyright (c) 2013年 ieModelingAndDesign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SampleXcode5Project/SampleXcode5ProjectTests/SampleXcode5ProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  SampleXcode5ProjectTests.m
+//  SampleXcode5ProjectTests
+//
+//  Created by git on 2013/11/13.
+//  Copyright (c) 2013年 ieModelingAndDesign. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SampleXcode5ProjectTests : XCTestCase
+
+@end
+
+@implementation SampleXcode5ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
